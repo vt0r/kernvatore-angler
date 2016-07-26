0531b7d6ccb8a37a7b9cdf423a41e101adce47bc
Tim Murray <timmurray@google.com>
2016-07-25 19:21:17 -0700

    kgsl: convert some workqueues to use kthreads
    adreno_dispatch_work and _kgsl_event_worker are both low-latency
    low-runtime functions that are in the critical path of GPU
    rendering. Moving them out of workqueues and into a dedicated FIFO
    kthread avoids significant jitter.
    
    bug 30342017
    
    Change-Id: I5feb4e829064d422b4b9af2acb449afd1f981899
    
     Conflicts:
    	drivers/gpu/msm/adreno_dispatch.c
    	drivers/gpu/msm/kgsl.h
    
    Fix conflicts.

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -25,6 +25,9 @@
 #include <linux/regulator/consumer.h>
 #include <linux/mm.h>
 #include <linux/dma-attrs.h>
+#include <linux/uaccess.h>
+#include <linux/kthread.h>
+#include <asm/cacheflush.h>
 
 /* The number of memstore arrays limits the number of contexts allowed.
  * If more contexts are needed, update multiple for MEMSTORE_SIZE
@@ -95,6 +98,10 @@ struct kgsl_driver {
 		unsigned int mapped_max;
 	} stats;
 	unsigned int full_cache_threshold;
+	struct workqueue_struct *workqueue;
+	struct workqueue_struct *mem_workqueue;
+	struct kthread_worker worker;
+	struct task_struct *worker_thread;
 };
 
 extern struct kgsl_driver kgsl_driver;
@@ -222,7 +229,7 @@ struct kgsl_event {
 	void *priv;
 	struct list_head node;
 	unsigned int created;
-	struct work_struct work;
+	struct kthread_work work;
 	int result;
 	struct kgsl_event_group *group;
 };

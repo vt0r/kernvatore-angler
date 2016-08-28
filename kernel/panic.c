a4125321ee749301cd54ace2c32e7aeb3fad7ddb
googyanas <googyanas@gmail.com>
2016-08-28 23:19:11 +0100

    Fix merge conflicts with 3.10.103

--- kernel/panic.c
+++ kernel/panic.c
@@ -145,6 +145,8 @@ void panic(const char *fmt, ...)
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
 
 	bust_spinlocks(0);
+	
+	console_flush_on_panic();
 
 	if (!panic_blink)
 		panic_blink = no_blink;

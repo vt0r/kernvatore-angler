a66873acc58e1635581703581dabbd19bccf30a0
Salvatore LaMendola <salvatore@lamendola.me>
2017-01-19 14:31:18 -0500

    [Ramdisk] - lkconfig update + init change - First update lkconfig to allow modification of the new power_efficient   workqueue sysctl added in the previous commits, as well as to restore   the user's preferred setting at boot. - Next, move the setting of this sysctl to the post boot script, in the   hopes that makes it occur late enough to actually work. - Finally, clean up the lkconfig script (tabs --> spaces). Who uses   tabs anyway?

--- kernvatore.anykernel2/ramdisk/sbin/kernvatore-post-boot.sh
+++ kernvatore.anykernel2/ramdisk/sbin/kernvatore-post-boot.sh
@@ -38,3 +38,9 @@ SFILE="/sys/devices/soc.0/f9924000.i2c/i2c-2/2-0070/input/input0/wake_gesture"
 CFILE="/data/data/leankernel/wlan_rx"
 SFILE="/sys/module/bcmdhd/parameters/wl_divide"
 [ -f $CFILE ] && echo `cat $CFILE` > $SFILE
+
+# power_efficient workqueue control
+CFILE="/data/data/leankernel/power_efficient"
+SFILE="/sys/module/workqueue/parameters/power_efficient"
+echo 1 > $SFILE
+[ -f $CFILE ] && echo `cat $CFILE` > $SFILE
